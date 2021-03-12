c762ded595b6a89b81f357f5a93d47b54fc9b2d0
C0n6w3i <32346233+C0n6w3i@users.noreply.github.com>
2021-03-12 11:01:22 -0500

    added events, added random num

--- contracts/SlotManagement.sol
+++ contracts/SlotManagement.sol
@@ -1,30 +1,35 @@
 pragma solidity ^0.8.0;
 import "./SlotFactory.sol";
 contract SlotManagement is SlotFactory {
+    event updateSlot(address indexed owner, uint slotID,uint cropID,uint grow_time,uint price,uint dry_time,uint grass_time,bool stealed, uint level);
+    event updateCropList(uint cropID,uint grow_time, uint level);
+    event deleteCropList(uint cropID);
     mapping (uint => Slot) public cropsList;
 
     modifier ownerOf(uint _slotID) {
         require(msg.sender == slotToOwner[_slotID]);
         _;
     }
-    // TODO: find a way to change 100 to some random number
     function _randomNumber(uint _limit) internal view returns (uint) {
-        return 100 % _limit;
+        return uint(keccak256(abi.encodePacked(msg.sender, block.timestamp))) % _limit;
+
     }
     function modInCropsList(uint _cropID, uint _grow_time, uint _price) external {
         cropsList[_cropID] = Slot(_cropID, _grow_time, _price, 0, 0, false, 1);
+        emit updateCropList(_cropID, _grow_time, _price);
     }
     function delInCropsList(uint _cropID) external {
         cropsList[_cropID] = Slot(0, 0, 0, 0, 0, false, 1);
+        emit deleteCropList(_cropID);
     }
     function _triggerGrow(Slot storage _slot) internal {
         _slot.grow_time = uint32(block.timestamp + (cropsList[_slot.cropID].grow_time * (10-_slot.level)));
     }
     function _triggerDry(Slot storage _slot) internal {
-        _slot.dry_time = uint32(block.timestamp + _randomNumber(_slot.grow_time - block.timestamp) + 10);
+        _slot.dry_time = uint32(block.timestamp + _randomNumber(_slot.grow_time - block.timestamp) + 100);
     }
     function _triggerGrass(Slot storage _slot) internal {
-        _slot.grass_time = uint32(block.timestamp + _randomNumber(_slot.grow_time - block.timestamp) + 10);
+        _slot.grass_time = uint32(block.timestamp + _randomNumber(_slot.grow_time - block.timestamp) + 100);
     }
     function _isReady(Slot storage _slot) internal view returns (bool) {
         return (_slot.grow_time <= block.timestamp);
@@ -42,9 +47,11 @@ contract SlotManagement is SlotFactory {
         mySlot.cropID = cropsList[_cropID].cropID;
         mySlot.grow_time = cropsList[_cropID].grow_time;
         mySlot.price = cropsList[_cropID].price;
+        mySlot.stealed = false;
         _triggerGrow(mySlot);
         _triggerDry(mySlot);
         _triggerGrass(mySlot);
+        emit updateSlot(msg.sender, _slotID, _cropID, mySlot.grow_time, mySlot.price, mySlot.dry_time, mySlot.grass_time, false, mySlot.level);
     }
     function harvest(uint _slotID) public ownerOf(_slotID){
         Slot storage mySlot = slots[_slotID];
@@ -61,17 +68,23 @@ contract SlotManagement is SlotFactory {
         }
         OwnerMoneyCount[msg.sender] = OwnerMoneyCount[msg.sender] + (mySlot.price * _tag);
         mySlot.cropID = 0;
+        emit updateSlot(msg.sender, _slotID, 0, 0, 0, 0, 0, false, mySlot.level);
+
     }
 
     function watering(uint _slotID) public ownerOf(_slotID){
         Slot storage mySlot = slots[_slotID];
         require(! _isReady(mySlot) && _isDry(mySlot));
         _triggerDry(mySlot);
+        emit updateSlot(msg.sender, _slotID, mySlot.cropID, mySlot.grow_time, mySlot.price, mySlot.dry_time, mySlot.grass_time, false, mySlot.level);
+
     }
     function weeding(uint _slotID) public ownerOf(_slotID){
         Slot storage mySlot = slots[_slotID];
         require(! _isReady(mySlot) && _isGrass(mySlot));
         _triggerGrass(mySlot);
+        emit updateSlot(msg.sender, _slotID, mySlot.cropID, mySlot.grow_time, mySlot.price, mySlot.dry_time, mySlot.grass_time, false, mySlot.level);
+
     }
     function steal(uint _slotID) public {
         Slot storage mySlot = slots[_slotID];
@@ -79,5 +92,7 @@ contract SlotManagement is SlotFactory {
         require(!mySlot.stealed);
         OwnerMoneyCount[msg.sender] = OwnerMoneyCount[msg.sender] + (mySlot.price);
         mySlot.stealed = true;
+        emit updateSlot(msg.sender, _slotID, mySlot.cropID, mySlot.grow_time, mySlot.price, mySlot.dry_time, mySlot.grass_time, true, mySlot.level);
+
     }
 }
